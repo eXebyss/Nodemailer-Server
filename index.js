63b06c814398bf09d19ca7c58ef3470bbe0c39f4
eXebyss <mysticlord27@gmail.com>
2022-01-21 14:00:36 +0200

    cors fix3

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const messageRouter = require('./routes/message.routes')
 
 const app = express()
 
-app.use(cors({ origin: '*' }))
+// app.use(cors({ origin: '*' }))
+app.use(function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+	res.setHeader('Access-Control-Allow-Credentials', true)
+	next()
+})
+
 app.use(express.json())
 
 app.use('/api', messageRouter)
