0aace03254cfe5f852a87af6754324150232e919
eXebyss <mysticlord27@gmail.com>
2022-08-13 02:33:41 +0300

    ExpressApolloServer: Apollo Server is added.

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+const { ApolloServer } = require('apollo-server-express')
+
 const express = require('express')
+
 const helmet = require('helmet')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const messageRouter = require('./routes/message.routes')
 const skillRouter = require('./routes/skill.routes')
+const typeDefs = require('./Apollo/schema')
+const resolvers = require('./Apollo/resolvers')
+const MessageAPI = require('./Apollo/dataSources/MessageAPI')
+const SkillAPI = require('./Apollo/dataSources/SkillAPI')
 
 const app = express()
 
@@ -14,18 +21,43 @@ app.use(express.json())
 app.use('/api/messages', messageRouter)
 app.use('/api/skills', skillRouter)
 
+async function startApolloServer(typeDefs, resolvers) {
+	const server = new ApolloServer({
+		typeDefs,
+		resolvers,
+		dataSources: () => {
+			return {
+				messageAPI: new MessageAPI(),
+				skillAPI: new SkillAPI(),
+			}
+		},
+		csrfPrevention: true,
+		cache: 'bounded',
+	})
+
+	await server.start()
+
+	server.applyMiddleware({
+		app,
+	})
+}
+
 const start = async () => {
 	try {
-		await mongoose.connect(process.env.DB_URL)
+		mongoose.connect(process.env.DB_URL)
 
-		app.listen(process.env.PORT || 3000, () => {
-			console.log(
-				`Nodemailer is listening at http://localhost:${process.env.PORT}`
-			)
+		app.listen(process.env.PORT || 4040, () => {
+			console.log(`
+				🚀  Server is running
+				🔉  Listening on port http://localhost:${process.env.PORT}
+				📭  Query at http://localhost:${process.env.PORT}/graphql
+			  `)
 		})
 	} catch (err) {
 		console.log(err)
 	}
 }
+
+startApolloServer(typeDefs, resolvers)
 
 start()
